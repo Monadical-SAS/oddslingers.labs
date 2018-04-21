ecc5ef1b9a02d5b2203bfb529f703b9826d25aeb
Nick Sweeting <git@nicksweeting.com>
2018-04-21 06:35:46 -0400

    better build command

--- app.py
+++ app.py
@@ -6,39 +6,29 @@ from datetime import datetime
 from flask import Flask, render_template
 
 
-CONFIG_FILE = 'content.json'
+### Config
 
+app = Flask(__name__)
+CONFIG_FILE = 'content.json'
 
 def load_config(fname=CONFIG_FILE):
     with open(fname, 'r') as f:
         lines = (l for l in f if not l.strip().startswith('//'))
         return json.loads('\n'.join(lines))
 
-def get_urls(config):
-    return list(config['PAGES'].items()) + list(config['POSTS'].items())
-
-
-
-app = Flask(__name__)
 CONFIG = load_config(CONFIG_FILE)
 
-def render_error(reason="That's an error.", status=404):
-    return render_template('error.html', **CONFIG, status=status, reason=reason)
+
+### Routes
 
 @app.route('/<name>.html')
 def render_page(name):
-    try:
-        page = CONFIG['PAGES'][name]
-    except KeyError:
-        return render_error('No page with that title.')
+    page = CONFIG['PAGES'][name]
     return render_template(page['template'], now=datetime.now(), **CONFIG, **page)
 
 @app.route('/posts/<name>.html')
 def render_posts(name):
-    try:
-        post = CONFIG['POSTS'][name]
-    except KeyError:
-        return render_error('No post with that title.')
+    post = CONFIG['POSTS'][name]
     return render_template(post['template'], now=datetime.now(), **CONFIG, **post)
 
 
